962bdb9056840d8db346f008121de7893be16d0d
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-04-03 22:34:32 -0600

    add state and event listener - fixing error about manifest

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ParticlesBg from 'particles-bg'
 import './App.css';
 import Navigation from './components/navigation/Navigation';
@@ -7,13 +7,21 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 function App() {
+  const [url, setUrl] = useState("");
+
+  const onInputChange = (event) => {
+    setUrl(event.target.value);
+  }
+
+  console.log(url);
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" color="#f9fcfc" num={250} bg={true}/>
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange = {onInputChange}/>
       {/* <FaceRecognition /> */}
 
     </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-function ImageLinkForm() {
+function ImageLinkForm({onInputChange}) {
+    const onButtonClick = () => {
+        console.log("clicked")
+    }
     return(
         <div>
             <p className='f3 fw5 white'>
                 {'This magic app will detect faces in your pictures. Try it!'}
             </p>
             <div className='form center pa4 br3'>
-                <input className='f4 pa2 w-70' type='text'></input>
-                <button className='pointer f4 w-30 link grow ph3 pv2 white bg-dark-blue'>Detect</button>
+                <input className='f4 pa2 w-70' type='text'
+                    onChange={onInputChange}
+                >
+                </input>
+                <button className='pointer f4 w-30 link grow ph3 pv2 white bg-dark-blue'
+                    onClick={onButtonClick}
+                >
+                    Detect
+                </button>
             </div>
         </div>
     )
