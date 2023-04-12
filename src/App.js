5cabcd6d078672f3f9056c90b2846b596ab64f44
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-04-12 17:24:02 -0600

    adding bface box

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 
 function App() {
-  const [url, setUrl] = useState("");
+  const [urlInput, setUrlInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [box, setBox] = useState({});
 
   const onInputChange = (event) => {
-    setUrl(event.target.value);
+    setUrlInput(event.target.value);
   }
 
   const setupRequestOptions = () => {
     const USER_ID = 'hoora_mir';
     const APP_ID = 'face-recognition';
-    const IMAGE_URL = url;
-    console.log(IMAGE_URL)
+    const IMAGE_URL = urlInput;
     const PAT = '39509417ced34ffa9e3bde36eb70b58c';
 
     const raw = JSON.stringify({
@@ -49,6 +50,8 @@ function App() {
   }
 
   const onButtonClick = () => {
+    setImageUrl(urlInput);
+
     const MODEL_ID = 'face-detection';
     const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
 
@@ -57,8 +60,29 @@ function App() {
     // this will default to the latest version_id
     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", setupRequestOptions())
       .then(response => response.json())
-      .then(result => console.log(result))
+      .then(result => displayBox(calculateFaceLocation(result)))
       .catch(error => console.log('error', error));
+  }
+
+  const calculateFaceLocation = (clarifiData) => {
+    const image = document.getElementById('image');
+    const height = Number(image.height);
+    const width = Number(image.width);
+    const boxLocation = clarifiData.outputs[0].data.regions[0].region_info.bounding_box;
+    const leftCol = width * boxLocation.left_col;
+    const topRow = height * boxLocation.top_row;
+    const rightCol = width - (width * boxLocation.right_col);
+    const bottomRow = height - (height * boxLocation.bottom_row);
+    return {
+      leftCol,
+      topRow,
+      rightCol,
+      bottomRow,
+    }
+  }
+
+  const displayBox = (dimentions) => {
+    setBox(dimentions);
   }
 
   return (
@@ -68,7 +92,7 @@ function App() {
       <Logo />
       <Rank />
       <ImageLinkForm onInputChange = {onInputChange} onButtonClick = {onButtonClick}/>
-      <FaceRecognition image = {url}/>
+      <FaceRecognition faceBox={box} image = {imageUrl}/>
 
     </div>
   );
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,19 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
+import './FaceRecognition.css';
 
-function FaceRecognition({image}) {
+function FaceRecognition({faceBox, image}) {
     return(
-        <div className='center mt2'>
-            <img src={image} alt='image' width='500px' height='auto'/>
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img id='image' src={image} alt='' width='500px' height='auto'/>
+                <div
+                    className='bounding-box'
+                    style={{left: faceBox.leftCol, top: faceBox.topRow, right:faceBox.rightCol, bottom: faceBox.bottomRow}}>
+                </div>
+            </div>
         </div>
     )
-
 }
 
 export default FaceRecognition;
